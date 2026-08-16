f47e0a514bfe08d35c92a7e00593c3c8c3be3c08
Anshu Gupta <anshug7784884036@gmail.com>
2026-08-16 17:09:41 +0530

    Time: 3 ms (14.61%), Space: 44.9 MB (21.64%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,17 +1,37 @@
+// class Solution {
+//     Set<String>set=new HashSet<>();
+//     public List<String> generateParenthesis(int n) {
+//         helper(n-1,"()");
+//         // helper(n,"");
+//         return new ArrayList<>(set);
+//     }
+//     public void helper(int n,String s){
+//         if(n==0){
+//             set.add(s);
+//             return;
+//         }
+//         int len=s.length();
+//         for(int i=0;i<len;i++){
+//             helper(n-1,s.substring(0,i)+"()"+s.substring(i));
+//         }
+//     }
+// }
 class Solution {
-    Set<String>set=new HashSet<>();
+    List<String> l;
     public List<String> generateParenthesis(int n) {
-        helper(n-1,"()");
-        return new ArrayList<>(set);
+        l=new ArrayList<>();
+        helper(n,n,"");
+        return l;
     }
-    public void helper(int n,String s){
-        if(n==0){
-            set.add(s);
+    public void helper(int open,int close,String s){
+        if(close<open || open<0 || close<0){
             return;
         }
-        int len=s.length();
-        for(int i=0;i<len;i++){
-            helper(n-1,s.substring(0,i)+"()"+s.substring(i));
+        if(close==0 && open==0){
+            l.add(s);
+            return;
         }
+        helper(open-1,close,s+'(');
+        helper(open,close-1,s+')');
     }
 }
